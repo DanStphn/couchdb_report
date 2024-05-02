a54699e9884b0ad47e75f0a82a455a67df427737
Daniel Stephen Alvarez <159105408+DanStphn@users.noreply.github.com>
2024-05-02 15:56:24 +0800

    Added Usage v5.2.15.56

--- src/pages/Usage.jsx
+++ src/pages/Usage.jsx
@@ -1,9 +1,52 @@
+import usageImage from '../assets/usage.png'
+
 const Usage = () => {
     return(
         <>
-            <div className="home_container">
-                <div className="home_content">
-                    Usage
+            <div className="usage_container">
+                <div className="usage_content">
+                    <div className="usage_text">
+                        <ul>
+                            <li>
+                                <div className="heading">
+                                    <img src={usageImage} alt="" />
+                                    <h1>There are many different ways to access CouchDB, here are the most common ways:</h1>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="usage">
+                                    <h2>Fauxton Web Interface</h2>
+                                    <ul>
+                                        <li>Open your web browser and navigate to the URL of your CouchDB instance <a href='http://localhost:5984/_utils' target='_blank'>http://localhost:5984/_utils</a>.</li>
+                                        <li>Log in to Fauxton using your CouchDB admin credentials.</li>
+                                        <li>From the Fauxton dashboard, you can manage databases, create and edit documents, run queries, and perform other administrative tasks using the graphical interface.</li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="usage">
+                                    <h2>HTTP API</h2>
+                                    <ul>
+                                        <li>Determine the URL of your CouchDB instance (e.g., http://localhost:5984).</li>
+                                        <li>For Web Apps, you could use nano. Nano provides a convenient interface directly from within Node.js for performing CRUD operations on your CouchDB database. For Node.js projects, just do <span>npm install nano</span>.</li>
+                                        <li>Use HTTP methods such as GET, PUT, POST, and DELETE to interact with CouchDB resources.</li>
+                                        <li>For example, to create a new database named "mydatabase", you would send a PUT request to <span>http://username:password@localhost:5984/mydatabase</span>.</li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="usage">
+                                    <h2>cURL and Command-Line Interface (CLI)</h2>
+                                    <ul>
+                                        <li>Download cURL from <a href="https://curl.se/download.html" target='_blank'>https://curl.se/download.html</a></li>
+                                        <li>Open a terminal or command prompt.</li>
+                                        <li>Use the curl command-line tool to send HTTP requests to your CouchDB instance.</li>
+                                        <li>For example, to create a new database named "mydatabase", you would run: <span>curl -X PUT http://username:password@localhost:5984/mydatabase</span>.</li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </>
